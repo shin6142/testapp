acb8442337d5d9b187a0dbd9f0e0541b7ea31520
shin6142 <shin6142>
2020-11-06 16:11:38 +0900

    Fixed the maximun letters of post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-    validates :content, {presence: true, length: {maximum: 140}}
+    validates :content, {presence: true}
     validates :user_id, {presence: true}
 
     def user
