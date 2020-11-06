62e379e9d9a266b7157b923adc4b8d32ce3bce23
shin6142 <shin6142>
2020-11-06 14:48:51 +0900

    functions added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,28 @@
 Rails.application.routes.draw do
+
+
+ 
+  root "home#top"
+
+  post "users/:id/update" => "users#update"
+  get "users/:id/edit" => "users#edit"
+  post "users/create" => "users#create"
+  get "signup" => "users#new"
+  get "users/index" => "users#index"
+  get "users/:id" => "users#show"
+  post "login" => "users#login"
+  get "logout" => "users#logout"
+  get "login" => "users#login_form"
+
+  
+
+  get "posts/index" => "posts#index"
+  get "posts/new" => "posts#new"
+  get "posts/:id" => "posts#show"
+  post "posts/create" => "posts#create"
+  get "posts/:id/edit" => "posts#edit"
+  post "posts/:id/update" => "posts#update"
+  post "posts/:id/destroy" => "posts#destroy"
+  get 'home/top' 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
