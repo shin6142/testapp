7fc8568f0624fa66a2e6c3481878ca21c6a07752
shin6142 <shin6142>
2021-02-02 12:57:56 +0900

    Add for deploy by capistrano

--- config/puma.rb
+++ config/puma.rb
@@ -36,3 +36,9 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+app_dir = File.expand_path("../..", __FILE__)
+bind "unix://#{app_dir}/tmp/sockets/puma.sock"
+pidfile "#{app_dir}/tmp/pids/puma.pid"
+state_path "#{app_dir}/tmp/pids/puma.state"
+stdout_redirect "#{app_dir}/log/puma.stdout.log", "#{app_dir}/log/puma.stderr.log", true
